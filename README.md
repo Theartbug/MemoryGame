d0f4fe25a61d5ab97edf0693639ae373349e3fc1
G.Provost <grace.g.provost@gmail.com>
2017-11-05 13:41:54 -0800

    create best score. Add leftover time to score when game finishes.

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 # MemoryGame
 
-A game to test my concepts: css selectors / animation, control flow for loops, if/else statements.s
+A game to test my concepts: css selectors / animation, control flow for loops, if/else statements.
+
+1. Create cards programatically for different levels.
+2. Animate cards to flip when clicked, light up when matched, and board game fade away when game ends.
+3. Create a timer that can accept different input in seconds and parse into minutes / seconds that counts down.
+4. Create a score system that rewards each match, subtracts each mis-flip, and add left over time.
+5. Create a best score system using local storage.
--- js/app.js
+++ js/app.js
@@ -2,12 +2,14 @@
 
 let cardsArray = [];
 let flippedCards = [];
+let scoreInStorage = localStorage.getItem('scoreInStorage');
 
 function gameMode() {
   let easybtn = document.getElementById('easy');
   let medbtn = document.getElementById('med');
   let hardbtn = document.getElementById('hard');
   let timer = document.querySelector('.timer');
+  setBestScore();
   if (easybtn) {
     easybtn.addEventListener('click', function() {
       startGame(5);
@@ -19,7 +21,7 @@ function gameMode() {
     })
     hardbtn.addEventListener('click', function() {
       startGame(15);
-      setTime(80, timer);
+      setTime(90, timer);
     })
   }
 } //gameMode
@@ -29,7 +31,6 @@ function gameMode() {
     createCardArray(numberOfCards);
     createCards();
     cardClick();
-    document.querySelector('.score').innerHTML = 'Score:<span>0</span>';
   } // startGame
 
   function clear() {
@@ -37,7 +38,8 @@ function gameMode() {
     cardsDiv.innerHTML = "";
     cardsDiv.classList.remove('select');
     cardsArray = [];
-  } // clear, resets flex direction
+    document.querySelector('.score').lastChild.textContent = 0;
+  } // clear, resets flex direction & score
 
   function createCardArray(numberOfCards) {
     for (let i = 1; i<= numberOfCards; i++) {
@@ -100,31 +102,6 @@ function gameMode() {
     flippedCards = [];
   } // flipBack
 
-  function endGame() {
-    let timer = document.querySelector('.timer').textContent;
-    if (document.getElementsByClassName('match').length === cardsArray.length || timer == '00:00') {
-      let cardsDiv = document.querySelector(".cards");
-      let timerDiv = document.querySelector('.timer');
-      setTimeout(function() {
-        cardsDiv.classList.add('slowFade');
-      }, 1200); // fade out gameboard
-      setTimeout(function() {
-        cardsDiv.classList.remove('slowFade');
-        cardsDiv.classList.add('select'); // reset flex direction
-          if (document.getElementsByClassName('match').length === cardsArray.length) {
-            cardsDiv.innerHTML = "<div class='win'>You Win! Play Again?</div>"
-          } // all matched
-          if (timer === '00:00') { // if timer ran out
-            cardsDiv.innerHTML = "<div class='win'>Time Out! Play Again?</div>"
-          } // timer ran out
-          cardsDiv.innerHTML += "<div class='win2'><button id='easy'>Easy</button> \
-          <button id='med'>Medium</button> \
-          <button id='hard'>Hard</button></div>"
-      }, 2300); // settimeout
-      setTimeout(gameMode,2301); //waits to run gameMode until buttons are created
-    } // if
-  } // endGame
-
 function setTime(duration, display) {
   let timer = duration, minutes, seconds;
   let interval = setInterval(function() {
@@ -152,43 +129,63 @@ function updateScore(points) {
   else if (parseInt(score.textContent) != 0) {
     score.textContent = parseInt(score.textContent) + parseInt(points);
   }
-} // score
 
-// function bestScore() {
-//   let score = document.querySelector('.score').lastChild;
-//   let bestScore = document.querySelector('.bestScore');
-//   let scoreInStorage = localStorage.getItem('bestScore');
-//   if ()
-// }
+  if (document.getElementsByClassName('match').length === cardsArray.length) { // add left over time if win
+    score.textContent = parseInt(score.textContent) + timeScore();
+  }
+} // updateScore
 
+function timeScore() {
+  let timeLeft = document.querySelector('.timer').textContent;
+  let convertedTime;
 
-// //SAVE SELECTED ITEMS ON WINDOW REFRESH
-//
-// window.onload = function () {
-//   let inStorageArray = localStorage.getItem('userSelectedDesires')
-//   if (inStorageArray != '' && inStorageArray != null) {
-//     userSelectedDesires = JSON.parse(inStorageArray);
-//     for (let i = 0; i < userSelectedDesires.length; i++) {
-//       document.getElementById(userSelectedDesires[i] + 'Img').className = 'active'; //for img
-//       document.getElementById(userSelectedDesires[i] + 'Img').nextElementSibling.className = 'selected'; //for span
-//       document.getElementById(userSelectedDesires[i]).checked = true; //for checkbox
-//     } // for
-//   } // if
-// } //window onload
-//
-// if (supportLocalStorage()) {
-//     if(UNform) {
-//       UNform.addEventListener('submit', function(event) {
-//       event.preventDefault();
-//       let userInput = document.getElementById('userName');
-//       localStorage.setItem('userName', userInput.value.toLowerCase());
-//       if (userInput.value == false) {
-//         alert('Please enter a name to proceed.');
-//       } else {
-//         toSelector();
-//       }
-//     })
-//   }
-// }
+  convertedTime = parseInt(timeLeft.charAt(3) + timeLeft.charAt(4));
+  convertedTime += (parseInt(timeLeft.charAt(1)) * 60);
+
+  return convertedTime;
+
+} // timeScore
+
+function setBestScore() {
+  let score = document.querySelector('.score').lastChild;
+  let bestScore = document.querySelector('.bestScore').lastChild;
+    if (!scoreInStorage) { // if no score in storage
+      localStorage.setItem('scoreInStorage', parseInt(score.textContent));
+      bestScore.textContent = parseInt(score.textContent);
+    }
+    if (parseInt(score.textContent) > parseInt(bestScore.textContent)) { // if score is higher than best score
+      localStorage.setItem('scoreInStorage', parseInt(score.textContent));
+      bestScore.textContent = parseInt(score.textContent);
+    }
+    if (JSON.parse(localStorage.getItem('scoreInStorage')) > parseInt(score.textContent)) { // if best score is higher than score
+      bestScore.textContent = JSON.parse(localStorage.getItem('scoreInStorage'))
+    }
+} // setBestScore
+
+function endGame() {
+  let timer = document.querySelector('.timer').textContent;
+  if (document.getElementsByClassName('match').length === cardsArray.length || timer == '00:00') {
+    let cardsDiv = document.querySelector(".cards");
+    let timerDiv = document.querySelector('.timer');
+    setTimeout(function() {
+      cardsDiv.classList.add('slowFade');
+    }, 1200); // fade out gameboard
+    setTimeout(function() {
+      cardsDiv.classList.remove('slowFade');
+      cardsDiv.classList.add('select'); // reset flex direction
+        if (document.getElementsByClassName('match').length === cardsArray.length) {
+          cardsDiv.innerHTML = "<div class='win'>You Win! Play Again?</div>"
+        } // all matched
+        if (timer === '00:00') { // if timer ran out
+          cardsDiv.innerHTML = "<div class='win'>Time Out! Play Again?</div>"
+        } // timer ran out
+        cardsDiv.innerHTML += "<div class='win2'><button id='easy'>Easy</button> \
+        <button id='med'>Medium</button> \
+        <button id='hard'>Hard</button></div>"
+        setBestScore();
+    }, 2300); // settimeout
+    setTimeout(gameMode,2301); //waits to run gameMode until buttons are created
+  } // if
+} // endGame
 
   gameMode();
